5579f27944c057047b9784752f80b1ce361b874a
Yuki1005 <90876725+Yuki1005@users.noreply.github.com>
2025-07-14 17:30:45 +0900

    Create a.py

---
+++ a.py
@@ -0,0 +1,22 @@
+import webbrowser
+
+lista = ["https://forms.office.com/r/pN0kTJPuy3",
+         "https://forms.office.com/r/sUNcUJGsB3",
+         "https://forms.office.com/r/mzTubRpaPC",
+         "https://forms.office.com/r/4N5KeUn19a",
+         "https://forms.office.com/r/iaqW5eJUxS",
+         "https://forms.office.com/r/Y0AsBepSjR",
+         "https://forms.office.com/r/EqfH5eAfsG",
+         "https://forms.office.com/r/ex9BaLtHn3",
+         "https://forms.office.com/r/vpPfQYZrbb",
+         "https://forms.office.com/r/aunBxyKmjV",
+         "https://forms.office.com/r/WJe9eFrjCD",
+        "https://forms.office.com/r/VJFpMTmW3y",
+        "https://forms.office.com/r/Jn5A2c2GYe",
+        "https://forms.office.com/r/CB4M7utpYK",
+        "https://forms.office.com/r/YJKtY6D1z3",
+        "https://forms.office.com/r/BvBLunwFES"]
+
+for i in lista:
+    # URLを開く
+    webbrowser.open(i)
